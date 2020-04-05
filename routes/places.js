3750c10ce0d71300ede41ff1d1ffe8c0d22e2d77
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2020-04-05 13:27:56 -0300

    Limpieza de rutas, debido a cambio de lógica de las relaciones.

--- routes/places.js
+++ routes/places.js
@@ -10,6 +10,5 @@ api.get('/places', md_auth.ensureAuth, PlacesController.getPlaces);
 api.post('/places', md_auth.ensureAuth, PlacesController.savePlace);
 api.put('/places/:id', md_auth.ensureAuth, PlacesController.updatePlace);
 api.delete('/places/:id', md_auth.ensureAuth, PlacesController.deletePlace);
-api.get('/places/tickets/:id/:validDate', md_auth.ensureAuth, PlacesController.countTickets);
 
 module.exports = api;
