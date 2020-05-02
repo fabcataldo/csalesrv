0c01ca2d78a3af44362228cb614829130d1d19ba
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2020-05-02 17:42:08 -0300

    Limpieza de código. Cambios en la implementación del pago con tarjeta en Mercado Pago.

--- controllers/mercado_pago_checkout/mercado_pago_checkout.js
+++ controllers/mercado_pago_checkout/mercado_pago_checkout.js
@@ -3,16 +3,21 @@ const mercadopago = require('mercadopago');
 var config = require('../../private_resources/config');
 var express = require('express');
 
+
+//crea un producto de preferencia, es lo que mi negocio quiere vender
+function preferenceProduct (req, res) {
+
 // Iniialize mercadopago SDK
 //mando los datos de mis credenciales (en este caso, copié las que estaban en los examples
 //del payment)
 mercadopago.configure({
-    client_id: config.client_id,
-    client_secret: config.client_secret
-  });
+  client_id: config.client_id,
+  client_secret: config.client_secret
+});
 
-//crea un producto de preferencia, es lo que mi negocio quiere vender
-function preferenceProduct (req, res) {
+mercadopago.configurations.setAccessToken("TEST-4810749615636979-022920-32ef3bbc2b8d5497662fe21661515b92-233164701");
+
+  /*
 // Crea un objeto de preferencia
 // CAMBIAR CUANDO HAGA LA APP MOVIL, va a ser un POST
 //envio el titulo del producto, cuanto sale o el price, y la cantidad (que viene de ticket)
@@ -36,7 +41,29 @@ var preference = {
   }).catch(function(error){
     console.log(error);
   });
+ */
 
+console.log(req.body);
+
+var payment_data = {
+  transaction_amount: req.body.transaction_amount,
+  //token: 'ff8080814c11e237014c1ff593b57b4d',
+  token: req.body.token,
+  description: req.body.description,
+  installments: 1,
+  payment_method_id: req.body.payment_method_id,
+  payer: {
+    email: 'test_user_30246109@testuser.com'
+  }
+};
+
+mercadopago.payment.save(payment_data).then(function (data) {
+      console.log(data);
+      res.status(200).send(data);
+    }).catch(function (error) {
+      console.log(error);
+      res.status(500).send(error);
+    });
 }
 
 module.exports={
--- routes/mercado_pago_checkout/mercado_pago_checkout.js
+++ routes/mercado_pago_checkout/mercado_pago_checkout.js
@@ -5,6 +5,6 @@ var api = express.Router();
 var md_auth = require('../../middlewares/authenticated');
 var MercadoPagoCheckoutController = require('../../controllers/mercado_pago_checkout/mercado_pago_checkout');
 
-api.get('/mercadopagopaymentprocess', md_auth.ensureAuth, MercadoPagoCheckoutController.preferenceProduct);
+api.post('/mercadopagopayment', md_auth.ensureAuth, MercadoPagoCheckoutController.preferenceProduct);
 
 module.exports = api;
