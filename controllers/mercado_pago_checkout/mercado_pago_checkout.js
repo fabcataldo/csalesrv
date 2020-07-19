0706908cbfdb34ce28b20a99f13a8be113036c32
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2020-07-19 15:10:17 -0300

    Mercado Pago payment: limpio código, arreglo bug en el pago con tarjeta.

--- controllers/mercado_pago_checkout/mercado_pago_checkout.js
+++ controllers/mercado_pago_checkout/mercado_pago_checkout.js
@@ -1,71 +1,59 @@
 'use strict'
 const mercadopago = require('mercadopago');
 const config = require('../../private_resources/config');
-var express = require('express');
 
 
-//crea un producto de preferencia, es lo que mi negocio quiere vender
-function preferenceProduct(req, res) {
-
-  // Iniialize mercadopago SDK
-  //mando los datos de mis credenciales (en este caso, copié las que estaban en los examples
-  //del payment)
+function mercadoPagoPayment(req, res) {
   mercadopago.configure({
     client_id: config.client_id,
     client_secret: config.client_secret
   });
 
   mercadopago.configurations.setAccessToken("TEST-4810749615636979-022920-32ef3bbc2b8d5497662fe21661515b92-233164701");
-
-  /*
-  // Crea un objeto de preferencia
-  // CAMBIAR CUANDO HAGA LA APP MOVIL, va a ser un POST
-  //envio el titulo del producto, cuanto sale o el price, y la cantidad (que viene de ticket)
-  //todo esto cuando haga el pago
-  var preference = {
-      items: [
-        {
-          title: 'Mi producto',
-          unit_price: 100,
-          quantity: 1,
-        }
-      ]
-    };
-  
-  mercadopago.preferences.create(preference)
-  .then(function(response){
-  // Este valor reemplazará el string "$$init_point$$" en tu HTML
-    global.init_point = response.body.init_point;
-
-    res.status(200).send({response})    
-  }).catch(function(error){
-    console.log(error);
-  });
- */
-
   if(req.body.payment_type == 'card'){
-    var payment_data = {
-      transaction_amount: req.body.transaction_amount,
-      //token: 'ff8080814c11e237014c1ff593b57b4d',
-      token: req.body.token,
-      description: req.body.description,
-      installments: 1,
-      payment_method_id: req.body.payment_method_id,
-      issuer_id: req.body.issuer_id,
-      payer: {
-        email: 'test_user_30246109@testuser.com'
-      }
-    };
+    customer_data = { "email": req.body.email }
 
-    mercadopago.payment.save(payment_data).then(function (data) {
-      console.log(data);
-      res.status(200).send(data);
+    mercadopago.customers.create(customer_data).then(function (customer) {
+      card_data = {
+        "token": req.body.token,
+        "customer": customer.id
+      }
+    
+      mercadopago.cards.create(card_data).then(function (card) {
+        var payment_data = {
+          transaction_amount: req.body.transaction_amount,
+          token: req.body.token,
+          description: req.body.description,
+          installments: 1,
+          payment_method_id: card.payment_method.id,
+          payer: {
+            email: customer_data
+          }
+        };
+        if(card.payment_method.name.includes("master")){
+          payment_data.issuer_id = card.issuer.id;
+        }
+    
+        mercadopago.payment.save(payment_data).then(function (data) {
+          console.log(data);
+          res.status(200).send(data);
+        }).catch(function (error) {
+          console.log(error);
+          res.status(500).send(error);
+        });
+      }).catch(function (error) {
+       console.log(error);
+       res.status(500).send(error);
+      });
+    
     }).catch(function (error) {
       console.log(error);
       res.status(500).send(error);
     });
   }
   else{
+    //recordar que, hay que enviar el email por el body
+    //acá está hardcodeado por razones de tesis
     var payment_data = {
       transaction_amount: req.body.transaction_amount,
       description: req.body.description,
@@ -84,5 +72,5 @@ function preferenceProduct(req, res) {
 }
 
 module.exports = {
-  preferenceProduct
+  mercadoPagoPayment
 }
--- routes/mercado_pago_checkout/mercado_pago_checkout.js
+++ routes/mercado_pago_checkout/mercado_pago_checkout.js
@@ -5,6 +5,6 @@ var api = express.Router();
 var md_auth = require('../../middlewares/authenticated');
 var MercadoPagoCheckoutController = require('../../controllers/mercado_pago_checkout/mercado_pago_checkout');
 
-api.post('/mercadopagopayment', md_auth.ensureAuth, MercadoPagoCheckoutController.preferenceProduct);
+api.post('/mercadopagopayment', md_auth.ensureAuth, MercadoPagoCheckoutController.mercadoPagoPayment);
 
 module.exports = api;
