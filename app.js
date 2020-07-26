d7697ebf397bc4fcc9640d6f8a38f8587b515604
fabcataldo <fabioalbertocataldo@gmail.com>
2020-07-26 11:55:49 -0300

    Agrego más seguridad a mi srv.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var bodyParser = require('body-parser');
 var cors = require('cors')
 const mercadopago = require('mercadopago');
 const config = require('./private_resources/config');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 var app = express();
 
 mercadopago.configure({
@@ -12,6 +16,26 @@ mercadopago.configure({
     client_secret: config.client_secret
 });
 mercadopago.configurations.setAccessToken("TEST-4810749615636979-022920-32ef3bbc2b8d5497662fe21661515b92-233164701");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json({limit: '1mb', entended: true}));
+app.use(cors())
+// configurar cabeceras http
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods: PUT,GET,POST,DELETE");
+    next();
+});
+const limit = rateLimit({
+    max: 100,// max requests
+    windowMs: 60 * 60 * 1000, // 1 Hour
+    message: 'Too many requests' // message to send
+});
+app.use('/routeName', limit);
+app.use(xss());
+app.use(helmet());
+app.use(mongoSanitize());
 
 // cargar rutas
 var users_routes = require('./routes/users');
@@ -25,19 +49,6 @@ var mercadopagocheckout_routes = require('./routes/mercado_pago_checkout/mercado
 var purchased_products_routes = require('./routes/purchased_products');
 var available_payment_methods_routes = require('./routes/available_payment_methods');
 var payment_methods_routes = require('./routes/payment_methods');
-
-app.use(bodyParser.urlencoded({extended:false}));
-//app.use(bodyParser.json());
-app.use(bodyParser.json({limit: '1mb', entended: true}));
-app.use(cors())
-// configurar cabeceras http
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods: PUT,GET,POST,DELETE");
-    next();
-});
-
 
 // rutas base
 app.use('/api', users_routes);
