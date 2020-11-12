498fd42d533bda551104f2bcf126825490dd187c
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2020-11-12 18:33:30 -0300

    Migro bd debido a baja en mlab, a atlas, y cambio el string de conexión al cluster de atlas de mi bd.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 3000;
 mongoose.Promise = global.Promise;
 
 //mongoose.connect('mongodb://localhost:27017/csaledb', {useNewUrlParser:true}, (err, res) => {
-mongoose.connect('mongodb://fabio1010:colo2020@ds119052.mlab.com:19052/heroku_3b3jcrqp', {useNewUrlParser:true}, (err, res) => {
+mongoose.connect('mongodb://csaleclouddb:colo2020@cluster-3b3jcrqp-shard-00-00.fi2b2.mongodb.net:27017,cluster-3b3jcrqp-shard-00-01.fi2b2.mongodb.net:27017,cluster-3b3jcrqp-shard-00-02.fi2b2.mongodb.net:27017/heroku_3b3jcrqp?ssl=true&replicaSet=atlas-ket7n1-shard-0&authSource=admin&retryWrites=true&w=majority', {useNewUrlParser:true}, (err, res) => {
 	if(err){
 		throw err;
 	}else{
