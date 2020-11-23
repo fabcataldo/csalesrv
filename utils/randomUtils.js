db8ab8e3897e408632d73ec00abe3b03624a997a
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2020-11-23 12:21:35 -0300

    Corrijo generación de código único para los detalles de compra, o tickets, en compra de productos.

--- utils/randomUtils.js
+++ utils/randomUtils.js
@@ -10,7 +10,7 @@ function makeRandomString(length) {
 
 function makeUniqueCode(length) {
 	var result = '';
-	var characters = '0123456789';
+	var characters = '123456789';
 	var charactersLength = characters.length;
 	for (var i = 0; i < length; i++) {
 		result += characters.charAt(Math.floor(Math.random() * charactersLength));
